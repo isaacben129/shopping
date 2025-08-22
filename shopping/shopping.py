2b7eaeef6f300bf970025df18c9c1bdef2a46b4c
isaacben129 <109280030+isaacben129@users.noreply.github.com>
2025-08-22 17:35:18 +0000

    Fri, Aug 22, 2025, 8:35 PM +03:00

--- shopping/shopping.py
+++ shopping/shopping.py
@@ -133,11 +133,16 @@ def evaluate(labels, predictions):
     representing the "true negative rate": the proportion of
     actual negative labels that were accurately identified.
     """
-    total_positive = len((y for y in predictions if y == 1))
-    total_negative = len((y for y in predictions if y == 0))
+    total_positive = 0
     positive_correct = 0
+    total_negative = 0
     negative_correct = 0
     for actual, predicted in zip(labels, predictions):
+        # counting totals
+        if predicted == 1:
+            total_positive += 1
+        elif predicted == 0:
+            total_negative += 1
         # counting number of correct totals
         if predicted == actual and predicted == 0:
             negative_correct += 1
